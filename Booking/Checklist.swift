9d68c140f784914ab0e9782ee8076e14e9349096
AlexFu <alexfu.jcase@gmail.com>
2016-11-08 18:53:12 +0800

    add Prototype

---
+++ Booking/Checklist.swift
@@ -0,0 +1,9 @@
+//
+//  checklist.swift
+//  Booking
+//
+//  Created by AlexFu on 2016/11/4.
+//  Copyright © 2016年 Razeware LLC. All rights reserved.
+//
+
+import Foundation
---
+++ Booking/SeriveListTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SeriveListTableViewController.swift
+//  Booking
+//
+//  Created by AlexFu on 2016/11/3.
+//  Copyright © 2016年 Razeware LLC. All rights reserved.
+//
+
+import Foundation
